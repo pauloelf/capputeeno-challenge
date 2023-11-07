74c153030ec885aa34e25f9d81c25fc6968d4a68
Paulo Sergio <contato.paulofws@gmail.com>
2023-11-07 14:25:24 -0300

    feat: add funcs to update and delete cart items

--- capputeeno/src/context/cart-context.tsx
+++ capputeeno/src/context/cart-context.tsx
@@ -24,6 +24,8 @@ export type ProductProps = {
 type CartContextProps = {
   cartProducts: ProductProps[]
   AddProductInCart: (product: ProductProps, id: string) => void
+  UpdateQuantityProduct: (quantity: number, id: string) => void
+  DeleteProduct: (id: string) => void
 }
 
 const CartContext = createContext({} as CartContextProps)
@@ -61,12 +63,37 @@ export function CartContextProvider({ children }: ProviderProps) {
     }
   }
 
+  const UpdateQuantityProduct = (quantity: number, id: string) => {
+    const products = [...cartProducts]
+    const newQuantity = products.map((product) => {
+      if (product.id !== id) return product
+      return { ...product, quantity }
+    })
+    localStorage.setItem('cart-products', JSON.stringify(newQuantity))
+    setCartProducts(JSON.parse(localStorage.getItem('cart-products') ?? ''))
+  }
+
+  const DeleteProduct = (id: string) => {
+    const products = [...cartProducts]
+    const newListProducts = products.filter((product) => product.id !== id)
+
+    localStorage.setItem('cart-products', JSON.stringify(newListProducts))
+    setCartProducts(JSON.parse(localStorage.getItem('cart-products') ?? ''))
+  }
+
   useEffect(() => {
     localStorage.setItem('cart-products', JSON.stringify([...cartProducts]))
   }, [cartProducts])
 
   return (
-    <CartContext.Provider value={{ cartProducts, AddProductInCart }}>
+    <CartContext.Provider
+      value={{
+        cartProducts,
+        AddProductInCart,
+        UpdateQuantityProduct,
+        DeleteProduct,
+      }}
+    >
       {children}
     </CartContext.Provider>
   )
